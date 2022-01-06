f8f2fb57266c86eb3d24a8a144b134cb43c1af4d
Muhammad Nazih Mahmudi <nazih.mahmudi12@gmail.com>
2022-01-06 14:20:23 +0700

    login

--- main.js
+++ main.js
@@ -6,50 +6,73 @@ function onSignIn(googleUser) {
   console.log('Family Name: ' + profile.getFamilyName());
   console.log("Image URL: " + profile.getImageUrl());
   console.log("Email: " + profile.getEmail());
-  window.location.href="/home.html"
+  window.location.href="/kameraConsume/home.html"
 }
 
-$("#tabel").ready(function () {
-	var tabel = document.getElementById("tabel")
-	getAll().then(response => {
-			console.log(response)
-			for(var i = 0; i <response.length; i++){
-					const tr = tabel.insertRow()
+function signOut() {
+	gapi.auth2.getAuthInstance().signOut().then(function() {
+		console.log('Signed out')
+		window.location.href="/kameraConsume/index.html";
+	})
+	.catch(error => {console.log(error)});
+}
+
+      function wsconsume() {
+        $.getXMLHttpRequest("http://localhost:8080/kameralist/kamera", function(data) {
+          console.log(data);
+          $.each(data,function(i){
+        	const tr = tabel.insertRow()
 					const td1 = tr.insertCell();
 					const td2 = tr.insertCell();
 					const td3 = tr.insertCell();
 					const td4 = tr.insertCell();
 					const td5 = tr.insertCell();
           const td6 = tr.insertCell();
-					console.log(response[i])
+					console.log(data[i])
 						
-					td1.innerHTML = response[i].merek
-					td2.innerHTML = response[i].tipe
-					td3.innerHTML = response[i].jenis
-					td4.innerHTML = response[i].harga
-					td5.innerHTML = response[i].noseri
+					td1.innerHTML = data[i].merek
+					td2.innerHTML = data[i].tipe
+					td3.innerHTML = data[i].jenis
+					td4.innerHTML = data[i].harga
+					td5.innerHTML = data[i].noseri
 					td6.innerHTML =`
           <div class ="justify content-center">
 					<a class="btn btn-info" href="edit.html?noseri=${response[i].noseri}">Edit</a>
 					<button type ="button" class=""btn btn-danger"" onclick="del(${response[i].noseri});">Delete</button>
 					</div>`
-					}
-			}
-	)
-});
+					})
+        })
+      }
 
-function signOut() {
-	var auth2 = gapi.auth2.getAuthInstance();
-	console.log(gapi.auth2);
-	auth2.signOut().then(function() {
-	alert("User signed out");
-	localStorage.removeItem("nama");
-	localStorage.removeItem("image");
-	localStorage.removeItem("email");
-	window.location.href="/";
-	})
-	.catch(error => {console.log(error)});
-}
+// $("#tabel").ready(function () {
+// 	var tabel = document.getElementById("tabel")
+// 	$.getJSON("http://localhost:8080/kameralist/kamera", function(data) {
+// 	// getAll().then(response => {
+// 			console.log(data)
+// 			$.each(data,function(i){
+// 					const tr = tabel.insertRow()
+// 					const td1 = tr.insertCell();
+// 					const td2 = tr.insertCell();
+// 					const td3 = tr.insertCell();
+// 					const td4 = tr.insertCell();
+// 					const td5 = tr.insertCell();
+//           const td6 = tr.insertCell();
+// 					console.log(data[i])
+						
+// 					td1.innerHTML = data[i].merek
+// 					td2.innerHTML = data[i].tipe
+// 					td3.innerHTML = data[i].jenis
+// 					td4.innerHTML = data[i].harga
+// 					td5.innerHTML = data[i].noseri
+// 					td6.innerHTML =`
+//           <div class ="justify content-center">
+// 					<a class="btn btn-info" href="edit.html?noseri=${response[i].noseri}">Edit</a>
+// 					<button type ="button" class=""btn btn-danger"" onclick="del(${response[i].noseri});">Delete</button>
+// 					</div>`
+// 					})
+// 			}
+// 	)
+// });
 
 function onLoad(){
 	gapi.load("auth2", function(){
@@ -63,7 +86,7 @@ function dlt(noseri){
 	if (window.confirm("Delete data?")===true) {
 			axios.delete(`http://localhost:8080/kameralist/kamera/${noseri}`).then((result) => {
 					alert("Success")
-					window.location.href="home.html"
+					window.location.href="/home.html"
 			}).catch((e) => {
 					console.log(e)
 			});
